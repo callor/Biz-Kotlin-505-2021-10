1ff2807d28c4942cd85152336e22069ad51c96a9
callor <callor@callor.com>
2021-12-24 09:43:23 +0900

    B

--- Kotlin_001/src/main/kotlin/Array_03.kt
+++ Kotlin_001/src/main/kotlin/Array_03.kt
@@ -1,13 +1,15 @@
 fun main() {
 
     val array = arrayListOf<Int>()
-
-
     val primes = array.filter {
-  
+        // it 이 소수인가 ? 판별하는 코드
+        // 소수가 맞다 return true
+        for (num in 2 until it) {
+            if (it % num == 0) {
+                break
+            }
+        }
+        true
     }
-
-}
-
 
 }
